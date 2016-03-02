c8764cdc89fd31de36dbb2f93b912f8c60e362f3
useenator <useenator@gmail.com>
2016-03-02 04:54:00 +0000

    Simple loader with caching .

--- app/src/main/java/com/example/useenator/myasyncloaderapplication1/StringsLoaderTask.java
+++ app/src/main/java/com/example/useenator/myasyncloaderapplication1/StringsLoaderTask.java
@@ -10,6 +10,7 @@ import java.util.List;
  * Created by hmed on 02/03/16.
  */
 public class StringsLoaderTask extends AsyncTaskLoader<List<String>> {
+    List<String> mCachedData;
 
     public StringsLoaderTask(Context context) {
         super(context);
@@ -17,13 +18,20 @@ public class StringsLoaderTask extends AsyncTaskLoader<List<String>> {
 
     @Override
     protected void onStartLoading() {
-        forceLoad();
+        //check if it's the the fist load.
+        if (mCachedData==null){
+            forceLoad();
+        }else{
+            this.deliverResult(mCachedData);
+        }
     }
 
     @Override
     public List<String> loadInBackground() {
         String[] data= getContext().getResources().getStringArray(R.array.strings_items);
         List<String> strings = Arrays.asList(data);
+        //make some delay
+        try {Thread.sleep(4000);} catch (InterruptedException e) {e.printStackTrace();}
 
 
         return strings;
@@ -31,6 +39,8 @@ public class StringsLoaderTask extends AsyncTaskLoader<List<String>> {
 
     @Override
     public void deliverResult(List<String> data) {
+        //fill the cache to prevent force loading
+        mCachedData=data;
         super.deliverResult(data);
     }
 }
